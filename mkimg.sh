3f7b5d626929b4975b77b0af7adc6195d99d24bd
Yang Le <yangle@pp.com>
2012-10-20 20:28:42 +0800

    脚本注释

--- mkimg.sh
+++ mkimg.sh
@@ -6,12 +6,12 @@ ld86 -0 -s -o boot boot.o
 as -o head.o head.s
 ld -s -x -M -Ttext 0x0 -e startup_32 -o system head.o > System.map
 
-# 制作空白软盘映像
+# 制作空白软盘映像（1024 * 1440 字节）
 dd bs=1024 if=/dev/zero of=fd.vfd count=1440
 
-# 挂载软盘到loop设备，并写入编译生成的文件
+# 关联软盘到loop设备，并写入编译生成的文件
 losetup /dev/loop0 fd.vfd
-dd bs=32 if=boot of=/dev/loop0 skip=1
-dd bs=512 if=system of=/dev/loop0 skip=8 seek=1
+dd bs=32 if=boot of=/dev/loop0 skip=1				# 跳过开头的32字节MINIX可执行文件头
+dd bs=512 if=system of=/dev/loop0 skip=8 seek=1		# 跳过4K的ELF可执行文件头，seek=1指定从512字节的位置写入
 sync
 losetup -d /dev/loop0
